aa694649685ae22eaa403201eb494405111da4b9
baojun_a <baojun_a@aspirecn.com>
2018-08-23 18:29:38 +0800

    新增用户名，密码，系统实体bean

---
+++ sso-server/src/main/java/sso/server/support/auth/UsernamePasswordSysCredential.java
@@ -0,0 +1,33 @@
+package sso.server.support.auth;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apereo.cas.authentication.RememberMeUsernamePasswordCredential;
+
+import javax.validation.constraints.Size;
+
+/**
+ * 用户名，密码，系统
+ *
+ */
+public class UsernamePasswordSysCredential extends RememberMeUsernamePasswordCredential {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    @Size(min = 2, message = "require system")
+    private String system;
+
+    public String getSystem() {
+        return system;
+    }
+
+    public UsernamePasswordSysCredential setSystem(String system) {
+        this.system = system;
+        return this;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().appendSuper(super.hashCode()).append(this.system).toHashCode();
+    }
+}
