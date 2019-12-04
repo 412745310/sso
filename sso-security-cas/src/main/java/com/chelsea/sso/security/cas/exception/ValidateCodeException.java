5c81853930cd42bf433c21c37a4cb093a4bfb943
baojun_a <baojun_a@aspirecn.com>
2019-12-04 20:55:18 +0800

    新增自定义异常类

---
+++ sso-security-cas/src/main/java/com/chelsea/sso/security/cas/exception/ValidateCodeException.java
@@ -0,0 +1,16 @@
+package com.chelsea.sso.security.cas.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class ValidateCodeException extends AuthenticationException {
+
+    public ValidateCodeException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
