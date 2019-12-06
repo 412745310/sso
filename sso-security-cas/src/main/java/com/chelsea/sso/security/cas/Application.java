6362fa91c0a5c69bbebd1aa8f7456161add634e4
baojun_a <baojun_a@aspirecn.com>
2019-12-06 08:59:58 +0800

    修改启动类

--- sso-security-cas/src/main/java/com/chelsea/sso/security/cas/Application.java
+++ sso-security-cas/src/main/java/com/chelsea/sso/security/cas/Application.java
@@ -2,10 +2,11 @@ package com.chelsea.sso.security.cas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
